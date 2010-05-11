3e7b288d02f6bc691a56d0d8e45d676363c2113e
Felix Rios <onceltuca@gmail.com>
2010-05-11 07:59:19 +0200

    realloc

--- malloc.h
+++ malloc.h
@@ -3,22 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NALLOC 1024
-typedef long Align; /* Ändra till rätt */
-
-
-union header{
-  struct{
-    union header *ptr;
-    size_t size; /* unisgned vad? */
-  } s;
-  Align x;
-};
-
-typedef union header Header;
-
 void *malloc(size_t size);
 void *realloc(void *ptr, size_t  size);
 void free(void *ptr);
-static Header *morecore(size_t nu);
+
 #endif
--- test.c
+++ test.c
@@ -1,7 +1,12 @@
 #include "malloc.h"
+#include <stdio.h>
 int main(int argc, char *argv[]){
+  fprintf(stderr,"\nmalloc(8*2048) (bytes = 2048 units) \n");
+  int * p = malloc(8*2048);
+  fprintf(stderr,"\nrealloc(p,8*8) (8 units)\n");
+  p = (int *)realloc(p,8*8);
+  fprintf(stderr,"\nmalloc 8*10\n");
+  p = malloc(8*10);
   
-  int * a  = (int *) malloc(sizeof(int));
-  *a = 2;
   return 0;
 }
