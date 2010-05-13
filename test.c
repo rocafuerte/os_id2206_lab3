997a3d24444de73a94253443cc69b68dbe5a6b81
Felix Rios <onceltuca@gmail.com>
2010-05-13 17:00:18 +0200

    new tests

--- test.c
+++ test.c
@@ -2,47 +2,16 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
-void testquick() { 
-    char *p1 = malloc(5);
-    fprintf(stderr,"p1: %d\n",p1);
-    strcpy(p1,"h2345");
-    p1 = realloc(p1,4711);
-}
-
-int main(int argc, char *argv[]){
-    testquick();
-   /* Från labpek */
-    /* fprintf(stderr,"------Test från labpek\n");    */
-/*     void * p = malloc(0); */
-/*     free(p); */
-/*     free(NULL); */
-/*     /\* detta ska ge tillbaka 0 == NULL *\/ */
-/*     #include <sys/resource.h> */
-/*     struct rlimit r; */
-/*     getrlimit(RLIMIT_DATA, &r); */
-/*     fprintf(stderr,"r.rlim_max: %d\n",r.rlim_max); */
-/*     p = malloc(2 * r.rlim_max); */
-/*       fprintf(stderr,"p ska vara null: %d\n",p); */
-    
-/*     p = realloc(NULL, 17); */
-/*     p = realloc(p,0); */
-/*     p = realloc(NULL,0); */
-    
-    /*
-    fprintf(stderr,"\nmalloc(8*2048) (bytes = 2048 units) \n");
-    p = malloc(8*1000);
-    p = malloc(8*1000);
-    p = malloc(8*1000);
-    fprintf(stderr,"\nrealloc(p,8*8) (8 units)\n");
-    p = (int *)realloc(p,8*10);
-    
-    fprintf(stderr,"\nmalloc 8*10\n");
-    p = malloc(8*10);
-    */
-    /*    malloc(5);
-    malloc(16);
-    malloc(0);
-    malloc(300);
-    */
+#define NITS 1000000
+#define MSIZE 8
+int r;
+int main(int argc, char *argv[]){    
+    int msize = 0;
+    char * addr[NITS];
+    int i;
+    for(i=0;i<NITS; ++i){        
+        msize = (i%MSIZE)+1;
+        addr[i] = malloc(msize);
+    }
     return 0;
 }
