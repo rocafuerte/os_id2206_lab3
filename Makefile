e078f93494adb3a58ce2cf675510d7e5fc06f05e
Felix Rios <onceltuca@gmail.com>
2010-05-18 21:58:20 +0200

    Before making quicklist to contain Headers instead of *Headers

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ worstfit:
 	gcc -o test $(FLAGS) -DSTRATEGY=3 malloc.c test.c
 
 quickfit:
-	gcc -o test $(FLAGS) -DSTRATEGY=4 -DNRQUICKLISTS=5 malloc.c test.c
+	gcc -o test $(FLAGS) -DSTRATEGY=4 -DNRQUICKLISTS=6 malloc.c test.c
 robban:
 	cp malloc.c test_os/ 
 	cd test_os && make 
--- test.c
+++ test.c
@@ -4,14 +4,33 @@
 #include <string.h>
 #define NITS 1000000
 #define MSIZE 8
-int r;
+#define TIMES 1000
 int main(int argc, char *argv[]){    
+    /*
     int msize = 0;
     char * addr[NITS];
     int i;
-    for(i=0;i<NITS; ++i){        
+    for(i=0;i<NITS; ++i){
         msize = (i%MSIZE)+1;
         addr[i] = malloc(msize);
     }
+    */
+
+/*     srand(time(NULL)); */
+/*     int i; */
+/*     double * p = malloc(245988); */
+/*     for(i=0;i<100000;i++){        */
+/*         /\*p = (double*)malloc((rand()%1024)*sizeof(double));        *\/ */
+/*         p = (double*)malloc((128+i)*sizeof(double)); */
+/*     } */
+    print_free_lists();
+    char * p = malloc(8*3);
+    print_free_lists();
+    p = malloc(8*4);
+    /*realloc(p,8*1);*/
+    /*realloc(p,0);
+      realloc(p,8*2);*/
+    
+    print_free_lists();
     return 0;
 }
